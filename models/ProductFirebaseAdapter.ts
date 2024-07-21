32bfcb94d6d5f79a20cae651ae50bad712c0da4b
jerojimenez17 <jerodepiran@gmail.com>
2024-07-21 19:50:34 -0300

    change name Adapter

---
+++ models/ProductFirebaseAdapter.ts
@@ -0,0 +1,38 @@
+import { DocumentData } from "firebase/firestore";
+import Product from "./Product";
+import noImg from "@/public/no-image.svg";
+
+export class ProductFirebaseAdapter {
+  public static fromDocumentDataArray(data: DocumentData[]): Product[] {
+    let state: Product[] = [];
+    data.forEach((d) => {
+      state.push(ProductFirebaseAdapter.fromDocumentData(d.data(), d.id));
+    });
+    return state;
+  }
+
+  public static fromDocumentData(data: DocumentData, dataId: string): Product {
+    let product = new Product();
+    product.id = dataId;
+    data.description
+      ? (product.description = data.description)
+      : (product.description = ""),
+      data.price ? (product.price = data.price) : (product.price = 0),
+      data.gain ? (product.gain = data.gain) : (product.gain = 0),
+      data.cod ? (product.cod = data.cod) : (product.cod = ""),
+      data.internCode
+        ? (product.internCode = data.internCode)
+        : (product.internCode = ""),
+      data.last_update
+        ? (product.last_update = data.last_update)
+        : (product.last_update = new Date()),
+      (product.salePrice = data.salePrice);
+    if (data.image.length === 0) {
+      product.image = "NoImg";
+      console.log(product.image);
+    }
+    product.image = data.image;
+
+    return product;
+  }
+}

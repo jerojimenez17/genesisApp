29b979dc3b94418bd66d9e0883daef3a644c12e4
jerojimenez17 <jerodepiran@gmail.com>
2024-07-21 19:56:10 -0300

    fix error and hide sections

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -4,12 +4,13 @@ import { NavigationMenu } from "@/components/ui/navigation-menu";
 
 const HomeLayout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
-  return (
-    <div className="h-full flex flex-col max-h-full items-center justify-center">
-      {/*bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-stone-200 to-emerald-400}*/}
-      <NavigationMenuHeader session={session} />
-      {children}
-    </div>
-  );
+  if (session)
+    return (
+      <div className="h-full flex flex-col max-h-full items-center justify-center">
+        {/*bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-stone-200 to-emerald-400}*/}
+        <NavigationMenuHeader session={session} />
+        {children}
+      </div>
+    );
 };
 export default HomeLayout;

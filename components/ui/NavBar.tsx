f451c98dc61f2467de1483a821db10ceef5cc853
jerojimenez17 <jerodepiran@gmail.com>
2024-07-21 19:56:20 -0300

    hide sections

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -71,25 +71,27 @@ export function NavigationMenuHeader({ session }: props) {
       </div>
       <NavigationMenu className="my-1 p-1">
         <NavigationMenuList>
-          <NavigationMenuItem className="">
-            <NavigationMenuTrigger className="backdrop-filter backdrop-blur-xl bg-gray font-bold text-white hover:bg-gray hover:backdrop-filter hover:backdrop-blur ">
-              Stock
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                {components.map((component) => (
-                  <ListItem
-                    className="font-semibold"
-                    key={component.title}
-                    title={component.title}
-                    href={component.href}
-                  >
-                    {component.description}
-                  </ListItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
+          {session.user.role === "ADMIN" && (
+            <NavigationMenuItem className="">
+              <NavigationMenuTrigger className="backdrop-filter backdrop-blur-xl bg-gray font-bold text-white hover:bg-gray hover:backdrop-filter hover:backdrop-blur ">
+                Stock
+              </NavigationMenuTrigger>
+              <NavigationMenuContent>
+                <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                  {components.map((component) => (
+                    <ListItem
+                      className="font-semibold"
+                      key={component.title}
+                      title={component.title}
+                      href={component.href}
+                    >
+                      {component.description}
+                    </ListItem>
+                  ))}
+                </ul>
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          )}
           <NavigationMenuItem>
             <NavigationMenuTrigger className="bg-gray backdrop-filter backdrop-blur-xl font-bold text-white hover:bg-gray hover:backdrop-filter hover:backdrop-blur-lg">
               Pedidos

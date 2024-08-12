35190bb04ee1fd560e908804427c072c428633dd
jerojimenez17 <jerodepiran@gmail.com>
2024-08-12 02:32:07 -0300

    wraping the client's table

--- components/client/client-table.tsx
+++ components/client/client-table.tsx
@@ -49,22 +49,24 @@ const ClientTable = ({ session }: props) => {
             />
             <ClientButtonModal />
           </div>
-          {clients
-            ?.filter((client) =>
-              client.name.toLowerCase().includes(searchClient.toLowerCase())
-            )
-            .map((client) => (
-              <div
-                onClick={() => {
-                  setClientSelect(client);
-                  console.log(client);
-                }}
-                className=" m-1 hover:odd:bg-opacity-40 before:bg-gray-300 h-12 font-semibold text-opacity-60 text-black justify-center flex flex-col odd:bg-white hover:even:bg-emerald-300 hover:even:bg-opacity-40  odd:bg-opacity-30 w-full"
-                key={client.id}
-              >
-                {client.name}
-              </div>
-            ))}
+          <div className="flex flex-col h-full w-full p-2 m-2 mb-6 overflow-auto">
+            {clients
+              ?.filter((client) =>
+                client.name.toLowerCase().includes(searchClient.toLowerCase())
+              )
+              .map((client) => (
+                <div
+                  onClick={() => {
+                    setClientSelect(client);
+                    console.log(client);
+                  }}
+                  className="hover:odd:bg-opacity-40 before:bg-gray-300 h-12 font-semibold text-opacity-60 text-black justify-center flex flex-col odd:bg-white hover:even:bg-emerald-300 hover:even:bg-opacity-40  odd:bg-opacity-30 w-full"
+                  key={client.id}
+                >
+                  {client.name}
+                </div>
+              ))}
+          </div>
         </>
       ) : (
         <div className="h-full">
